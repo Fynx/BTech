340c6b3edd845780fd591cba2bc44469f5a70d83
Bartosz Szreder <szreder@mimuw.edu.pl>
2014-06-23 21:05:40 +0200

    Reduce insanity coefficient in toUnderlyingRef()

--- src/BTCommon/EnumSerialization.h
+++ src/BTCommon/EnumSerialization.h
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2013 Bartosz Szreder
+Copyright (c) 2013,2014 Bartosz Szreder
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -35,7 +35,7 @@ constexpr typename std::underlying_type <ET>::type toUnderlying(ET et)
 template <typename ET>
 constexpr typename std::underlying_type <ET>::type & toUnderlyingRef(ET &et)
 {
-	return reinterpret_cast<typename std::add_lvalue_reference<decltype(toUnderlying(et))>::type>(et);
+	return reinterpret_cast<typename std::underlying_type<ET>::type &>(et);
 }
 
 #endif
